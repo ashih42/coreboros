f88810b3e86f09aec18e6e67daaaf655cc375216
Andy Shih <34532080+ashih42@users.noreply.github.com>
2026-08-22 13:08:02 -0500

    Delete unused code.

--- src/warrior/warrior_metadata.rs
+++ src/warrior/warrior_metadata.rs
@@ -1,5 +1,3 @@
-use std::{fmt, path::PathBuf};
-
 #[derive(Debug, Clone)]
 pub struct WarriorMetadata {
     pub name: String,
@@ -14,49 +12,5 @@ impl Default for WarriorMetadata {
             author: None,
             strategy: None,
         }
-    }
-}
-
-impl WarriorMetadata {
-    /// Construct `WarriorMetadata` with `name` derived from `filepath`.
-    /// Example: filepath: "warriors/doge.red" -> name: "doge"
-    #[allow(
-        clippy::missing_panics_doc,
-        clippy::unwrap_used,
-        reason = "This operation is guaranteed to succeed because an earlier operation to open and read this file has succeeded."
-    )]
-    #[must_use]
-    pub fn from_file(filepath: &str) -> Self {
-        assert!(!filepath.is_empty());
-
-        let pathbuf = PathBuf::from(filepath);
-
-        let name = pathbuf
-            .file_prefix()
-            .unwrap()
-            .to_string_lossy()
-            .into_owned();
-
-        Self {
-            name,
-            author: None,
-            strategy: None,
-        }
-    }
-}
-
-impl fmt::Display for WarriorMetadata {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        const WIDTH: usize = 15;
-
-        let author = self.author.as_deref().unwrap_or_default();
-        let strategy = self.strategy.as_deref().unwrap_or_default();
-
-        writeln!(f, ";redcode\n")?;
-        writeln!(f, "{:<WIDTH$}{}", ";name", self.name)?;
-        writeln!(f, "{:<WIDTH$}{}", ";author", author)?;
-        writeln!(f, "{:<WIDTH$}{}", ";strategy", strategy)?;
-
-        Ok(())
     }
 }
