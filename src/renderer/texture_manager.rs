b7d28ca0e431b189ae60965bfdbd3fc5cf827b10
Andy Shih <34532080+ashih42@users.noreply.github.com>
2026-08-22 15:21:02 -0500

    Add missing textures for warriors 4 - 7.

--- src/renderer/texture_manager.rs
+++ src/renderer/texture_manager.rs
@@ -11,6 +11,11 @@ pub struct TextureManager {
     digit_1: Option<egui::TextureHandle>,
     digit_2: Option<egui::TextureHandle>,
     digit_3: Option<egui::TextureHandle>,
+    digit_4: Option<egui::TextureHandle>,
+    digit_5: Option<egui::TextureHandle>,
+    digit_6: Option<egui::TextureHandle>,
+    digit_7: Option<egui::TextureHandle>,
+    digit_8: Option<egui::TextureHandle>,
 }
 
 /// Convert bytes to image, and register the image with the UI context.
@@ -44,6 +49,11 @@ impl TextureManager {
         init_tex!(self, egui_ctx, digit_1);
         init_tex!(self, egui_ctx, digit_2);
         init_tex!(self, egui_ctx, digit_3);
+        init_tex!(self, egui_ctx, digit_4);
+        init_tex!(self, egui_ctx, digit_5);
+        init_tex!(self, egui_ctx, digit_6);
+        init_tex!(self, egui_ctx, digit_7);
+        init_tex!(self, egui_ctx, digit_8);
     }
 
     pub fn get_warrior_icon(&self, warrior_id: WarriorId) -> Option<&egui::TextureHandle> {
@@ -52,6 +62,10 @@ impl TextureManager {
             1 => self.digit_1.as_ref(),
             2 => self.digit_2.as_ref(),
             3 => self.digit_3.as_ref(),
+            4 => self.digit_4.as_ref(),
+            5 => self.digit_5.as_ref(),
+            6 => self.digit_6.as_ref(),
+            7 => self.digit_7.as_ref(),
             _ => None,
         }
     }
