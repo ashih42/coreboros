5a1ed4544fb1687026394f13efb1a1249034bd99
Andy Shih <34532080+ashih42@users.noreply.github.com>
2026-08-22 13:04:19 -0500

    Make an input redcode missing the ";name" metadata comment compiled with name: "_default_name".

--- src/warrior/warrior_metadata.rs
+++ src/warrior/warrior_metadata.rs
@@ -1,10 +1,20 @@
 use std::{fmt, path::PathBuf};
 
-#[derive(Debug, Default, Clone)]
+#[derive(Debug, Clone)]
 pub struct WarriorMetadata {
     pub name: String,
     pub author: Option<String>,
     pub strategy: Option<String>,
+}
+
+impl Default for WarriorMetadata {
+    fn default() -> Self {
+        Self {
+            name: "_default_name".to_owned(),
+            author: None,
+            strategy: None,
+        }
+    }
 }
 
 impl WarriorMetadata {
